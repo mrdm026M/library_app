b605e5e082b184f8da9ba66130789cac69f81f43
Dhruv Maheshwari <mrdm026@gmail.com>
2021-04-27 13:42:13 +0530

    created color class

---
+++ lib/data/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const ColorGreen = Color(0xFF28D063);
+  static const ColorBlue = Color(0xFF34334B);
+  static const ColorPink = Color(0xFFFD8369);
+  static const ColorWhite = Color(0xFFF3F5F9);
+}
