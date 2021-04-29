777a35bb0b20ab317adbd3ff04f14db02613c6c3
Dhruv Maheshwari <mrdm026@gmail.com>
2021-04-29 15:19:10 +0530

    added new color

--- lib/data/colors.dart
+++ lib/data/colors.dart
@@ -6,5 +6,6 @@ class AppColors {
   static const ColorGreen = Color(0xFF28D063);
   static const ColorBlue = Color(0xFF34334B);
   static const ColorPink = Color(0xFFFD8369);
+  static const ColorGrey = Colors.grey;
   static const ColorWhite = Color(0xFFF3F5F9);
 }
