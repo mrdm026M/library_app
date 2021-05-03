0178ac8668607b5b2a55659ce07d2a51ff667bae
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-03 17:40:28 +0530

    new screens

---
+++ lib/Screens/AudioScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AudioScreen extends StatelessWidget {
+  const AudioScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Audio'),
+    );
+  }
+}
---
+++ lib/Screens/BookScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookScreen extends StatelessWidget {
+  const BookScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Book'),
+    );
+  }
+}
